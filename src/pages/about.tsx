fbd2e1daa9f21d0aff07383ce782870c66767aef
Juan Martin Rodriguez <jmartinrodriguez.work@gmail.com>
2023-04-27 16:22:09 -0300

    ADD: initial features

---
+++ src/pages/about.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = (): JSX.Element => {
+  return (
+    <div className='h-screen w-full flex justify-center items-center'>
+      About page
+    </div>
+  )
+}
+
+export default About
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,13 @@
+import Head from 'next/head'
+
 export default function Home (): JSX.Element {
   return (
-    <main className="h-screen w-full flex items-center justify-center">
-      <h1>Palta app</h1>
+    <main className="h-screen w-full flex flex-col gap-2 items-center justify-center">
+      <Head>
+        <title>Paltas</title>
+        <link rel="shortcut icon" href="faviconjm.ico" type="image/x-icon" />
+      </Head>
+      <h1 className='text-4xl'>Palta app</h1>
     </main>
   )
 }
