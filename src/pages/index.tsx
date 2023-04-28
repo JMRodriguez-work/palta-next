68a02fbb8749ab9f27e767b4f45eeba3a49dad7a
Juan Martin Rodriguez <jmartinrodriguez.work@gmail.com>
2023-04-28 14:01:07 -0300

    ADD: fetching allProducts and ProductById

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,24 @@
 import Head from 'next/head'
 import Navbar from '@/components/Navbar'
+import { useEffect, useState } from 'react'
 
 export default function Home (): JSX.Element {
+  const [productList, setProductList] = useState<TProduct[]>([])
+
+  useEffect(() => {
+    const getProducts = async (): Promise<void> => {
+      try {
+        const response = await fetch('/api/avo')
+        const data = await response.json()
+        setProductList(data.data)
+      } catch (error) {
+        console.error(error)
+      }
+    }
+    getProducts()
+      .catch(console.error)
+  }, [])
+
   return (
     <main className="h-screen w-full">
       <Head>
@@ -9,6 +26,9 @@ export default function Home (): JSX.Element {
         <link rel="shortcut icon" href="faviconjm.ico" type="image/x-icon" />
       </Head>
       <Navbar />
+      {productList?.map(item => (
+        <div key={item.id}>{item.name}</div>
+      ))}
     </main>
   )
 }
--- src/pages/product/[productId].tsx
+++ src/pages/product/[productId].tsx
@@ -1,12 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
+import Navbar from '@/components/Navbar'
 
 export default function ProductItem (): JSX.Element {
   const { query: { productId } } = useRouter()
+  const [product, setProduct] = useState<TProduct>()
+
+  useEffect(() => {
+    const getProduct = async (): Promise<void> => {
+      const response = await fetch(`/api/avo/${productId as string}`)
+      const data = await response.json()
+      setProduct(data)
+    }
+    getProduct().catch(console.error)
+  }, [productId])
 
   return (
     <div>
+        <Navbar />
         <p>Pagina del producto: { productId }</p>
+        {product?.name}
     </div>
   )
 }
