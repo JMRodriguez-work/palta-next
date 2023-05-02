4c6f7c5e8916db16ff5e95feae963e03915a342d
Juan Martin Rodriguez <jmartinrodriguez.work@gmail.com>
2023-05-02 13:07:49 -0300

    FIX: warning viewport in _document

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,11 +3,15 @@ import type { AppProps } from 'next/app'
 import Layout from '@/components/Layout'
 import { Provider } from 'react-redux'
 import { store } from '@/redux/store'
+import Head from 'next/head'
 
 export default function App ({ Component, pageProps }: AppProps): any {
   return (
     <Provider store={store}>
       <Layout>
+        <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        </Head>
         <Component {...pageProps} />
       </Layout>
     </Provider>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,6 @@ export default function Document (): JSX.Element {
     <Html lang='es'>
       <Head>
         <link rel="shortcut icon" href="faviconjm.ico" type="image/x-icon" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <body>
         <Main />
